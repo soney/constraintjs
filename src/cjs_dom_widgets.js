8f78d6be954a97c00f221bf3f04335e7b8181bf4
Stephen Oney <swloney@gmail.com>
2012-02-16 18:30:02 -0500

    Added file

---
+++ src/cjs_dom_widgets.js
@@ -0,0 +1,93 @@
+(function(cjs, root) {if(!cjs._is_node) {
+var _ = cjs._;
+
+var mouse = {
+	x: cjs.constraint()
+	, y: cjs.constraint()
+};
+
+root.addEventListener("mousemove", function(event) {
+	mouse.x.set(event.pageX);
+	mouse.y.set(event.pageY);
+});
+
+cjs.mouse = mouse;
+
+
+var modifiers = {alt: false, ctrl: false, shift: false};
+var keyboard = {
+	pressed: cjs.constraint([])
+	, modifiers : cjs.constraint(modifiers)
+};
+
+
+var update_modifiers = function(event) {
+	var new_modifiers = {
+		alt: event.altKey
+		, ctrl: event.ctrlKey
+		, shift: event.shiftKey
+	};
+
+	if(!_.isEqual(new_modifiers, modifiers)) {
+		modifiers = new_modifiers;
+		keyboard.modifiers.set(modifiers);
+	}
+};
+
+var pressed_keys = [];
+root.addEventListener("keydown", function(event) {
+	update_modifiers(event);
+
+	var keyCode = event.keyCode;
+	pressed_keys = _.union(pressed_keys, [keyCode]);
+	keyboard.pressed.set(pressed_keys);
+});
+
+root.addEventListener("keyup", function(event) {
+	update_modifiers(event);
+
+	var keyCode = event.keyCode;
+	pressed_keys = _.without(pressed_keys, keyCode);
+	keyboard.pressed.set(pressed_keys);
+});
+
+cjs.keyboard = keyboard;
+
+cjs.time = cjs.constraint(function() {
+	return (new Date()).getTime();
+});
+root.setInterval(_.bind(cjs.time.nullify, cjs.time), 10);
+
+
+cjs.bind = function(obj, prop_name, constraint) {
+	var update = function(value) {
+		obj[prop_name] = value;
+	};
+
+	update(constraint.get());
+	constraint.onChange(update);
+
+	return _.bind(cjs.unbind, cjs, constraint, update);
+};
+cjs.unbind = function(constraint, update) {
+	constraint.offChange(update);
+};
+
+cjs.css = function(elem, prop_name, constraint) {
+	var name = _.camel_case(prop_name);
+	return cjs.bind(elem.style, name, constraint);
+};
+
+cjs.attr = function(elem, prop_name, constraint) {
+	return cjs.bind(elem, prop_name, constraint);
+};
+
+cjs.text = function(elem, constraint) {
+	return cjs.bind(elem, "textContent", constraint);
+};
+
+cjs['class'] = function(elem, constraint) {
+	return cjs.bind(elem, "className", constraint);
+};
+
+}}(cjs, this));
