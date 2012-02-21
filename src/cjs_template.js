62094af183aa25422b2e5fbbd6c345993f43d56f
Stephen Oney <swloney@gmail.com>
2012-02-21 17:24:35 -0500

    Branching

--- src/cjs_template.js
+++ src/cjs_template.js
@@ -18,10 +18,10 @@
 				"var p=[],print=function(){p.push.apply(p,arguments);};" +
 				
 				//Introduce the data as local variables using with(){}
-				"with(obj){p.push('"+
+				"with(obj){p.push('"
 
 				//Convert the template into pure JavaScript
-				str	.replace(/[\r\t\n]/g, " ")
+				+str.replace(/[\r\t\n]/g, " ")
 					.split("<%").join("\t")
 					.replace(/((^|%>)[^\t]*)'/g, "$1\r")
 					.replace(/\t=(.*?)%>/g, "',$1,'")
@@ -32,8 +32,8 @@
 				+ "');}return p.join('');");
 
 			//Provide some basic currying to the user
-			return data ? fn(data) : fn;
-		};
+		return data ? fn(data) : fn;
+	};
 
 	cjs.define("template", templ);
 }(cjs));
