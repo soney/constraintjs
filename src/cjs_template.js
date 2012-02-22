fa3c706558e250fcdd58e35e5dde2905c8f38aa9
Stephen Oney <swloney@gmail.com>
2012-02-22 14:01:52 -0500

    Added template parser

--- src/cjs_template.js
+++ src/cjs_template.js
@@ -1,38 +1,25 @@
 /*jslint evil: true regexp: true*/
-/*global document:true */
+/*global console: true, document:true */
 (function(cjs) {
-	//var _ = cjs._;
-	var cache = {};
-	//Similar to John Resig's implementation
-	var templ = function(str, data) {
-		//Figure out if we're getting a template or if we need to
-		//// load the template - and be sure to cache the result
-		var fn = !/\W/.test(str) ? 
-					cache[str] = cache[str] ||
-						templ(document.getElementById(str).innerHTML) :
-			
-			//Generate a reusable function that will serve as a template
-			//generator (and which will be cached)
-			//
-			new Function("obj",
-				"var p=[],print=function(){p.push.apply(p,arguments);};" +
-				
-				//Introduce the data as local variables using with(){}
-				"with(obj){p.push('"
+	var _ = cjs._;
 
-				//Convert the template into pure JavaScript
-				+str.replace(/[\r\t\n]/g, " ")
-					.split("<%").join("\t")
-					.replace(/((^|%>)[^\t]*)'/g, "$1\r")
-					.replace(/\t=(.*?)%>/g, "',$1,'")
-					.split("\t").join("');")
-					.split("%>").join("p.push('")
-					.split("\r").join("\\'")
-
-				+ "');}return p.join('');");
-
-			//Provide some basic currying to the user
-		return data ? fn(data) : fn;
+	var templ = function(str) {
+		var results = "";
+		_.html_parser(str, {
+				start: function(tag, attrs, unary) {
+				},
+				end: function(tag) {
+				},
+				chars: function(text) {
+				},
+				comment: function(text) {
+				},
+				startHandlebars: function(tag, attrs, block) {
+				}
+				, endHandlebars: function(tag) {
+				}
+			});
+		console.log(results);
 	};
 
 	cjs.define("template", templ);
