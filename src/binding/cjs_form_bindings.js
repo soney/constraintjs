74d60c34950e3f808528ae7b6cf16b7f3c91832b
Stephen Oney <swloney@gmail.com>
2012-03-13 13:57:33 -0400

    Switching comps

--- src/binding/cjs_form_bindings.js
+++ src/binding/cjs_form_bindings.js
@@ -1,9 +1,10 @@
 /*global document:true */
 (function(cjs) {if(!cjs._is_node) {
 cjs.binding.mixin({
-	val: function(elem, constraint) {
+	val: function(elems, constraint) {
 		if(arguments.length === 1) {
 			//Return a constraint whose value is my val
+			return cjs.create("input_value_constraint", _.first(elems));
 		} else {
 			return cjs.binding.bind(elem, "value", constraint);
 		}
