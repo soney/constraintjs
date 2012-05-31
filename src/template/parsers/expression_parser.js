fb3f3087a42eaf7a2f141fa5c3117ee99790fdd2
Stephen Oney <swloney@gmail.com>
2012-05-31 02:07:13 -0400

    Parser improvements

--- src/template/parsers/expression_parser.js
+++ src/template/parsers/expression_parser.js
@@ -22,24 +22,62 @@ var Parser = function(expr, options) {
 (function(my) {
 	var proto = my.prototype;
 	proto.tokenize = function() {
-		var node;
+		var rv = [];
 		while(this.buffer) {
-			this.buffer = ltrim(this.buffer);
-			node = this.gobble_expression();
-			if(node) {
-				this.curr_node = node;
-			} else {
+			this.gobble_expression();
+			if(this.curr_node === false) {
 				throw new Error("Unexpected " + this.buffer);
 			}
+			rv.push(this.curr_node);
+			console.log(this.curr_node);
 		}
+		if(rv.length === 1) {
+			return rv[0]
+		} else {
+			return {
+				type: "compound"
+				, statements: rv
+			};
+		}
+	};
+
+	proto.gobble_expression = function() {
+		var node;
+
+		do  {
+			this.buffer = ltrim(this.buffer);
+			node = false;
+			if(node === false) {
+				node = this.gobble_token()
+			}
+			if(node === false) {
+				node = this.parse_fn_call()
+			}
+			if(node === false) {
+				node = this.parse_parens()
+			}
+			if(node === false) {
+				node = this.parse_binary_op()
+			}
+			if(node === false) {
+				node = this.parse_unary_op()
+			}
+
+			if(node) {
+				this.curr_node = node;
+			}
+		} while(node);
+
+
 		return this.curr_node;
 	};
 
 	proto.gobble_token = function() {
-		this.buffer = ltrim(this.buffer);
 		var node = false;
 		if(node === false) {
-			node = this.parse_variable();
+			if(this.curr_node === null) {
+				node = this.parse_variable();
+			}
 		}
 		if(node === false) {
 			node = this.parse_dot_property();
@@ -50,15 +88,7 @@ var Parser = function(expr, options) {
 		if(node === false) {
 			node = this.parse_constant();
 		}
-		if(node === false) {
-			node = this.parse_fn_call();
-		}
 		return node;
-	};
-	proto.gobble_expression = function() {
-		var node;
-		node = this.gobble_token();
-		return this.gobble_token();
 	};
 
 	var var_regex = new RegExp("^([A-Za-z_$][A-Za-z_$0-9]*)");
@@ -121,7 +151,7 @@ var Parser = function(expr, options) {
 		}
 		return false;
 	};
-	var number_regex = new RegExp("^(\\d+(.\\d+)?)");
+	var number_regex = new RegExp("^(\\d+(\\.\\d+)?)");
 	proto.parse_constant = function() {
 		var match;
 		if(match = this.buffer.match(number_regex)) {
@@ -151,24 +181,24 @@ var Parser = function(expr, options) {
 		}
 		return false;
 	};
-	var fn_call_regex = new RegExp("^\\(");
+	var open_paren_regex = new RegExp("^\\(");
 	var fn_arg_regex = new RegExp("^\\s*,");
-	var fn_end_arg_regex = new RegExp("^\\s*\\)");
+	var close_paren_regex = new RegExp("^\\s*\\)");
 	proto.parse_fn_call = function() {
 		if(this.curr_node && (this.curr_node.type === "prop" || this.curr_node.type === "var" || this.curr_node.type === "fn_call")) {
 			var match;
-			if(match = this.buffer.match(fn_call_regex)) {
+			if(match = this.buffer.match(open_paren_regex)) {
 				var arg_node = false;
 				this.buffer = this.buffer.substr(1); // Kill the open paren
 				var args = [];
 				var old_curr_node = this.curr_node;
 				do {
 					this.curr_node = null;
-					arg_node = this.gobble_token();
+					arg_node = this.gobble_expression();
 					args.push(arg_node);
 					if(match = this.buffer.match(fn_arg_regex)) {
 						this.buffer = this.buffer.substr(match[0].length);
-					} else if(match = this.buffer.match(fn_end_arg_regex)) {
+					} else if(match = this.buffer.match(close_paren_regex)) {
 						this.buffer = this.buffer.substr(match[0].length);
 						break;
 					}
@@ -180,6 +210,73 @@ var Parser = function(expr, options) {
 					, fn: this.curr_node
 				};
 				return node;
+			}
+		}
+		return false;
+	};
+
+	var starts_with = function(str, substr) {
+		return str.substr(0,substr.length) === substr;
+	};
+
+	proto.parse_parens = function() {
+		var match;
+		if(match = this.buffer.match(open_paren_regex)) {
+			this.buffer = this.buffer.substr(match[0].length);
+			var previous_node = this.curr_node;
+			this.curr_node = null;
+			var contents = this.gobble_expression();
+			if(match = this.buffer.match(close_paren_regex)) {
+				this.buffer = this.buffer.substr(match[0].length);
+				var node = {
+					type: "grouping"
+					, contents: contents
+				};
+				return node;
+			} else {
+				throw new Error("Unclosed (");
+			}
+		}
+		return false;
+	};
+	proto.parse_unary_op = function() {
+		var i, leni;
+		for(i = 0, leni = unary_ops.length; i<leni; i++) {
+			var unary_op = unary_ops[i];
+			if(starts_with(this.buffer, unary_op)) {
+				this.buffer = this.buffer.substr(unary_op.length);
+				var operand = this.gobble_expression();
+
+				var node = {
+					type: "op"
+					, subtype: "unary"
+					, op: unary_op
+					, operands: [operand]
+				};
+				return node;
+			}
+		}
+	};
+	proto.parse_binary_op = function() {
+		if(this.curr_node !== null) {
+			var i, len;
+			for(i = 0, len = binary_ops.length; i<len; i++) {
+				var binary_op = binary_ops[i];
+				if(starts_with(this.buffer, binary_op)) {
+					this.buffer = this.buffer.substr(binary_op.length);
+					var operand_1 = this.curr_node;
+					this.curr_node = null;
+					var operand_2 = this.gobble_expression();
+
+
+					var node = {
+						type: "op"
+						, subtype: "binary"
+						, op: binary_op
+						, operands: [operand_1, operand_2]
+					};
+					return node;
+				}
 			}
 		}
 		return false;
--- test/unit_tests/expression_parser_test.js
+++ test/unit_tests/expression_parser_test.js
@@ -1,10 +1,33 @@
+
+(function() {
+var parse = cjs._.bind(cjs.__parsers.expression, cjs.__parsers);
+var test_parser = function(inp, out) {
+	var actual_out = {};
+	var parse_val = parse(inp);
+	cjs._.forEach(out, function(prop_val, prop_name) {
+		actual_out[prop_name] = parse_val[prop_name];
+	});
+	console.log(parse_val);
+	return deepEqual(actual_out, out);
+};
+
 module("Expression Parser");
 
-test('Variables', function() {
-	var parse = cjs._.bind(cjs.__parsers.expression, cjs.__parsers);
-	var test_parser = function(inp, out) {
-		return deepEqual(parse(inp), out);
-	};
-	var parse_result = parse("a(b(c, d), e)");
-	console.log(parse_result);
+test('Constants', function() {
+	test_parser("'abc'", {value: "abc"});
+	test_parser('"abc"', {value: "abc"});
+	test_parser("123", {value: 123});
+	test_parser("12.3", {value: 12.3});
 });
+
+test('Variables', function() {
+	test_parser("abc", {var_name: "abc"});
+});
+
+test('bleh', function() {
+	test_parser("a(b, c(d,e), f)", {});
+	test_parser("a b + c", {});
+});
+
+
+}());
