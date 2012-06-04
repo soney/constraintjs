220a3551fc9081050b6587677adebcd60cb04afe
Stephen Oney <swloney@gmail.com>
2012-06-04 16:45:29 -0400

    bleh

--- src/template/cjs_template.js
+++ src/template/cjs_template.js
@@ -8,6 +8,27 @@
 	cjs.__template_builders = {};
 
 
-	cjs.template = function(template_type, str, data, options) {
+	cjs.template = function(a,b,c,d) {
+		var template_type = "handlebars"
+			, str
+			, data = {}
+			, options = {};
+		if(arguments.length === 1) {
+			str = a;
+		} else if(arguments.length === 2) {
+			str = a;
+			data = b;
+		} else if(arguments.length === 3) {
+			str = a;
+			data = b;
+			options = c;
+		} else  {
+			template_type = a;
+			str = b;
+			data = c;
+			options = d;
+		} 
+
+		return cjs.__template_builders[template_type](str, data, options);
 	};
 }(cjs));
