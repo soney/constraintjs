80a18c0b93bbb797d6bbac555d1e6302a85eeddc
Stephen Oney <swloney@gmail.com>
2012-06-16 16:53:07 -0400

    Added dist builder

--- Makefile.dryice.js
+++ Makefile.dryice.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+var args = process.argv.slice(2)
+var command = args[0];
+
+if(!command) {
+	command = "build";
+}
+
 var cjs_inc = require('./include_libs');
 
 var do_build_main = function() {
@@ -21,9 +28,25 @@ var do_build = function() {
 var copy = require('dryice').copy;
 
 if(require.main === module) { //Called directly
-	console.log("Building...");
-	do_build();
-	console.log("Done!");
+	if(command === "build") {
+		console.log("Building...");
+		do_build();
+		console.log("Done!");
+	} else if(command === "dist") {
+		//http://stackoverflow.com/questions/5754153/zip-archives-in-node-js
+		var spawn = require('child_process').spawn
+			, fs = require('fs');
+
+		var package_info = JSON.parse(fs.readFileSync("package.json")); 
+		var version = package_info.version;
+		var filename = "dist/cjs_"+version+".zip";
+		var zip = spawn('zip', ['-rj', filename, "build/cjs.js", "build/cjs.min.js"], {cwd: __dirname});
+		zip.on('exit', function (code) {
+			if(code !== 0) {
+				console.log('zip process exited with code ' + code);
+			}
+		});
+	}
 } else { //required
 	exports.build = function(callback) {
 		do_build();
