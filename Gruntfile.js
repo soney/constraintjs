d524809226ba2e8d8677d5e47a5f576a586ebe76
Stephen Oney <swloney@gmail.com>
2013-04-12 10:21:21 -0700

    Switched to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+
+	var cjs_inc = require('./include_libs');
+
+	var package = grunt.file.readJSON('package.json');
+	// Project configuration.
+	grunt.initConfig({
+		pkg: package,
+		jshint: {
+			build: {
+				src: cjs_inc.main_src
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+						'<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */',
+				global_defs: {
+					DEBUG: false
+				}
+			},
+			build: {
+				src: cjs_inc.main_src,
+				dest: "build/cjs.min.js"
+			}
+		},
+		concat: {
+			options: {
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+						'<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */',
+				process: {
+					data: {
+						version: package.version,
+						build_time: grunt.template.today("yyyy-mm-dd h:MM:ss TT")
+					}
+				}
+			},
+			js: {
+				src: cjs_inc.main_src,
+				dest: "build/cjs.js"
+			}
+		},
+		clean: {
+			build: ["build/"]
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	// Default task(s).
+	grunt.registerTask('default', ['concat', 'uglify']);
+	grunt.registerTask('test', ['jshint']);
+};
--- include_libs.js
+++ include_libs.js
@@ -19,11 +19,7 @@ var c = concat = function() {
 };
 
 var path = "";
-var build_path = cp(path, "build");
 var src = cp(path, "src");
-var vendor_src = cp(src, "vendor");
-
-exports.main_build = cp(build_path, ["cjs.min.js"]);
 
 exports.main_src = c(
 	cp(src, [
@@ -40,26 +36,3 @@ exports.main_src = c(
 		*/
 	])
 );
-
-var core_tests_dir = cp(path, "test/core/unit_tests");
-exports.core_tests = cp(core_tests_dir, [	"util_tests.js"
-											, "primitive_type_tests.js"
-											]);
-
-var ends_with = function(str1, str2) {
-	return str1.slice(str1.length-str2.length) == str2;
-};
-exports.include_templates = function(strs) {
-	var do_it = function(str) {
-		if(ends_with(str, ".js")) {
-			return "<script type = 'text/javascript' src = '"+str+"'></script>";
-		} else if(ends_with(str, ".css")) {
-			return "<link rel = 'stylesheet' href = '"+str+"' media='screen' />";
-		}
-	};
-	if(typeof strs === "string") {
-		return do_it(strs);
-	} else {
-		return strs.map(do_it).join("");
-	}
-};
