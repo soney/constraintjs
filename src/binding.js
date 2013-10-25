3c6d36ba4d7a4293ca93fb5b0087af51c3c5da59
Stephen Oney <swloney@gmail.com>
2013-10-25 13:42:34 -0400

    Fixing children binding

--- src/binding.js
+++ src/binding.js
@@ -1,3 +1,12 @@
+	var make_node = function(item) {
+		if(_.isElement(item) || _.isTextElement(item) || _.isCommentElement(item)) {
+			return item;
+		} else {
+			var node = document.createTextNode(item);
+			return node;
+		}
+	};
+
 	var insert_at = function(child_node, parent_node, index) {
 		var children = parent_node.childNodes;
 		if(children.length <= index) {
@@ -74,7 +83,12 @@
 		this._throttle_delay = false;
 		this._timeout_id = false;
 
-		var curr_value, last_value = init_val;
+		var curr_value, last_value;
+		if(isFunction(init_val)) {
+			last_value = init_val(targets[0]);
+		} else {
+			last_value = init_val;
+		}
 		var old_targets = [];
 		this._do_update = function() {
 			this._timeout_id = false;
@@ -131,83 +145,89 @@
 		};
 	}(Binding));
 
-	var text_binding = function(elements) {
-		var args = slice.call(arguments, 1);
-		var val = cjs(function() {
+
+
+	var create_list_binding = function(list_binding_getter, list_binding_setter, list_binding_init_value) {
+		return function(elements) {
+			var args = slice.call(arguments, 1);
+			var val = cjs(function() {
+				return list_binding_getter(args);
+			});
+
+			var binding = new Binding({
+				targets: elements,
+				getter: function() { return val.get(); },
+				setter: function(element, value, old_value) {
+					list_binding_setter(element, value, old_value);
+				},
+				init_val: list_binding_init_value
+			});
+			return binding;
+		};
+	};
+	
+	var create_textual_binding = function(setter) {
+		return create_list_binding(function(args) {
 			var arg_val_arr = map(args, function(arg) {
 				return cjs.get(arg) + "";
 			});
 
 			return arg_val_arr.join("");
+		}, function(element, value) {
+			element.textContent = value;
 		});
-
-		var binding = new Binding({
-			targets: elements,
-			getter: function() { return val.get(); },
-			setter: function(element, value) {
-				element.textContent = value;
-			}
-		});
-		return binding;
 	};
+	var text_binding = create_textual_binding(function(element, value) {
+		element.textContent = value;
+	});
+	var html_binding = create_textual_binding(function(element, value) {
+		element.innerHTML = value;
+	});
+	var val_binding = create_textual_binding(function(element, value) {
+		element.val = value;
+	});
 
-	var class_binding = function(elements) {
-		var args = slice.call(arguments, 1);
-		var val = cjs(function() {
-			var arg_val_arr = map(args, function(arg) {
-				var arg_val = cjs.get(arg);
-				if(isArray(arg_val)) {
-					return arg_val;
-				} else {
-					return [arg_val];
-				}
-			});
 
-			return flatten(arg_val_arr, true);
+	var class_binding = create_list_binding(function(args) {
+		var arg_val_arr = map(args, function(arg) {
+			return cjs.get(arg);
 		});
-		var old_value = val.get();
 
-		var binding = new Binding({
-			targets: elements,
-			getter: function() { return val.get(); },
-			setter: function(element, value, old_value) {
-				var ad = get_array_diff(old_value, value);
-				var curr_class_name = " " + element.className + " ";
-				var added_classes = map(ad.added, function(added_info) {
-					return added_info.item;
-				}).join(" ");
-				each(ad.removed, function(removed_info) {
-					curr_class_name = curr_class_name.replace(" " + removed_info.from_item + " ", " ");
-				});
-				curr_class_name += added_classes;
-				curr_class_name = curr_class_name.trim();
-				element.className = curr_class_name;
-			},
-			init_val: []
+		return flatten(arg_val_arr, true);
+	}, function(element, value, old_value) {
+		var ad = get_array_diff(old_value, value);
+		var curr_class_name = " " + element.className + " ";
+		var added_classes = map(ad.added, function(added_info) {
+			return added_info.item;
+		}).join(" ");
+		each(ad.removed, function(removed_info) {
+			curr_class_name = curr_class_name.replace(" " + removed_info.from_item + " ", " ");
 		});
-		return binding;
-	};
+		curr_class_name += added_classes;
+		curr_class_name = curr_class_name.trim();
+		element.className = curr_class_name;
+	}, []);
 
-	var children_binding = function(elements) {
-		var args = slice.call(arguments, 1);
-		var val = cjs(function() {
-			var arg_val_arr = map(args, function(arg) {
-				return cjs.get(arg) + "";
-			});
-
-			return arg_val_arr.join(" ");
+	var children_binding = create_list_binding(function(args) {
+		var arg_val_arr = map(args, function(arg) {
+			return cjs.get(arg);
 		});
-		var old_value = val.get();
 
-		var binding = new Binding({
-			targets: elements,
-			getter: function() { return val.get(); },
-			setter: function(element, value) {
-				element.textContent = value;
-			}
+		return map(flatten(arg_val_arr, true), make_node);
+	}, function(element, value, old_value) {
+		var ad = get_array_diff(old_value, value);
+		each(ad.removed, function(removed_info) {
+			remove_index(element, removed_info.index);
 		});
-		return binding;
-	};
+		each(ad.added, function(added_info) {
+			insert_at(added_info.item, elem, x.index);
+		});
+		each(ad.moved, function(moved_info) {
+			move_child(element, moved_info.to_index, moved_info.from_index);
+		});
+	}, function(element) {
+		return toArray(element.childNodes);
+	});
 
 	var create_obj_binding = function(obj_binding_setter) {
 		return function(elements) {
@@ -256,8 +276,10 @@
 
 	extend(cjs, {
 		"text": text_binding,
-		"attr": attr_binding,
-		"css": css_binding,
+		"html": html_binding,
+		"val": val_binding,
 		"class": class_binding,
-		"children": children_binding
+		"children": children_binding,
+		"attr": attr_binding,
+		"css": css_binding
 	});
