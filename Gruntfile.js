0e4957cda43585e9b0700285a58a38999eb99ac4
Stephen Oney <swloney@gmail.com>
2013-11-25 14:55:22 -0500

    Added production option in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,10 @@
 module.exports = function(grunt) {
-	var package = grunt.file.readJSON('package.json'); // Project configuration.
-
-	var src_files = ["src/util.js", "src/core.js", "src/array.js", "src/map.js", "src/liven.js", "src/memoize.js", "src/binding.js",
-					"src/state_machine/cjs_fsm.js", "src/state_machine/cjs_events.js", "src/state_machine/cjs_fsm_constraint.js",
-					"src/template/jsep.js", "src/template/cjs_parser.js", "src/template/cjs_template.js"];
-	var enclosed_src_files = (["src/header.js"]).concat(src_files, "src/footer.js");
+	var package = grunt.file.readJSON('package.json'), // Project configuration.
+		src_files = ["src/util.js", "src/core.js", "src/array.js", "src/map.js", "src/liven.js",
+					"src/memoize.js", "src/binding.js", "src/state_machine/cjs_fsm.js",
+					"src/state_machine/cjs_events.js", "src/state_machine/cjs_fsm_constraint.js",
+					"src/template/jsep.js", "src/template/cjs_parser.js","src/template/cjs_template.js"],
+		enclosed_src_files = (["src/header.js"]).concat(src_files, "src/footer.js");
 
 	grunt.initConfig({
 		pkg: package,
@@ -14,19 +14,47 @@ module.exports = function(grunt) {
 			}
 		},
 		uglify: {
-			options: {
-				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-						'<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
-				report: 'gzip'
+			development: {
+				options: {
+					banner: '// v<%= pkg.version %>',
+					report: 'gzip',
+					sourceMapIn: "build/cjs.js.map",
+					sourceMap: "build/cjs.min.js.map",
+					sourceMappingURL: "cjs.min.js.map"
+				},
+				src: "build/cjs.js", // Use concatenated files
+				dest: "build/cjs.min.js"
 			},
-			build: {
+			production: {
+				options: {
+					banner: '//<%= pkg.version %>',
+					sourceMap: "build/cjs.min.js.map",
+					sourceMappingURL: "cjs.min.js.map",
+					sourceMapRoot: '..'
+				},
 				src: "build/cjs.js", // Use concatenated files
 				dest: "build/cjs.min.js"
 			}
 		},
+		concat_sourcemap: {
+			options: {
+				banner: '/*!<%= pkg.name %> - v<%= pkg.version %> - ' +
+						'<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+				process: {
+					data: {
+						version: package.version // the updated version will be added to the concatenated file
+					}
+				},
+				sourceRoot: '..'
+			},
+			js: {
+				src: enclosed_src_files,
+				dest: "build/cjs.js"
+			}
+		},
 		concat: {
 			options: {
-				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+				banner: '/*!<%= pkg.name %> - v<%= pkg.version %> - ' +
 						'<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
 				process: {
 					data: {
@@ -46,8 +74,31 @@ module.exports = function(grunt) {
 			build: ["build/"]
 		},
 		watch: {
-			files: src_files.concat(['test/unit_tests.js', 'test/unit_tests/*.js']),
-			tasks: ['concat', 'jshint', 'qunit']
+			test: {
+				files: src_files.concat(['test/unit_tests.js', 'test/unit_tests/*.js']),
+				tasks: ['concat_sourcemap', 'jshint', 'qunit']
+			},
+			quickdev: {
+				files: src_files,
+				tasks: ['concat_sourcemap']
+			},
+			full: {
+				files: src_files.concat(['test/unit_tests.js', 'test/unit_tests/*.js']),
+				tasks: ['concat_sourcemap', 'jshint', 'qunit', 'uglify']
+			}
+		},
+		compress: {
+			production: {
+				options: {
+					archive: 'ConstraintJS-<%= pkg.version %>.zip'
+				},
+				files: [{
+					expand: true,
+					cwd: 'build/',
+					src: '*',
+					dest: 'cjs'
+				}]
+			}
 		}
 	});
 
@@ -55,11 +106,15 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-concat-sourcemap');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compress');
 
 	// Default task(s).
-	grunt.registerTask('default', ['concat', 'jshint', 'qunit', 'uglify']);
-	grunt.registerTask('test', ['concat', 'jshint', 'qunit']); // Skip uglification if just testing
+	grunt.registerTask('default', ['concat_sourcemap', 'jshint', 'qunit', 'uglify:development']);
+	grunt.registerTask('dev', ['concat_sourcemap', 'jshint', 'qunit', 'uglify:development', 'watch:full']);
+	grunt.registerTask('quickdev', ['concat_sourcemap', 'watch:quickdev']);
+	grunt.registerTask('package', ['clean', 'jshint', 'concat', 'qunit', 'uglify:production', 'compress']);
 };
