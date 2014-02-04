da7db860234eefd2477d0122607491cba80d17f6
Stephen Oney <swloney@gmail.com>
2014-02-04 15:19:42 -0500

    Upgraded cjs version

--- builds/constraintjs-beta/cjs.js
+++ builds/constraintjs-beta/cjs.js
@@ -4461,7 +4461,11 @@ var text_binding = create_textual_binding(function(element, value) { // set the 
 	 *     });
 	 */
 	attr_binding = create_obj_binding(function(element, key, value) {
-		element.setAttribute(key, value);
+		if(fillAttrs[key] && !value) { // attributes like disabled that should be there or not
+			element.removeAttribute(key);
+		} else {
+			element.setAttribute(key, value);
+		}
 	});
 
 var inp_change_events = ["keyup", "input", "paste", "propertychange", "change"],
@@ -5331,10 +5335,11 @@ var makeMap = function(str){
 };
 
 // Regular Expressions for parsing tags and attributes
-var startTag = /^<([\-A-Za-z0-9_]+)((?:\s+[a-zA-Z0-9_\-]+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)\s*(\/?)>/,
+var startTag = /^<([\-A-Za-z0-9_]+)((?:\s+[a-zA-Z0-9_\-]+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|(?:[^>\s]+)))?)*)\s*(\/?)>/,
 	endTag = /^<\/([\-A-Za-z0-9_]+)[^>]*>/,
 	handlebar = /^\{\{([#=!>|{\/])?\s*((?:(?:"[^"]*")|(?:'[^']*')|[^\}])*)\s*(\/?)\}?\}\}/,
 	attr = /([\-A-Za-z0-9_]+)(?:\s*=\s*(?:(?:"((?:\\.|[^"])*)")|(?:'((?:\\.|[^'])*)')|([^\/>\s]+)))?/g,
+	//hb_attr = /\{\{([^\}]*)\}\}/g,
 	HB_TYPE = "hb",
 	HTML_TYPE = "html";
 	
@@ -5912,6 +5917,7 @@ var child_is_dynamic_html		= function(child)	{ return child.type === UNARY_HB_TY
 	hb_regex = /^\{\{([^\}]+)\}\}/,
 	get_constraint = function(str, context, lineage) {
 		var has_constraint = false,
+			has_str = false,
 			strs = [],
 			index, match_val, len = 0, substr,
 			last_val_is_str = false;
@@ -5945,21 +5951,22 @@ var child_is_dynamic_html		= function(child)	{ return child.type === UNARY_HB_TY
 			} else {
 				strs[len++] = substr;
 			}
-			last_val_is_str = true;
+			has_str = last_val_is_str = true;
 		}
 
 		if(has_constraint) {
-			return cjs(function() {
-				return map(strs, function(str) {
-					if(is_constraint(str)) {
-						return str.get();
-					} else if(is_array(str)) {
-						return str.join(" ");
-					} else {
-						return "" + str;
-					}
-				}).join("");
-			});
+			return (!has_str && strs.length===1) ? strs[0] :
+					cjs(function() {
+						return map(strs, function(str) {
+							if(is_constraint(str)) {
+								return str.get();
+							} else if(is_array(str)) {
+								return str.join(" ");
+							} else {
+								return "" + str;
+							}
+						}).join("");
+					});
 		} else {
 			return strs.join("");
 		}
@@ -6016,18 +6023,19 @@ var child_is_dynamic_html		= function(child)	{ return child.type === UNARY_HB_TY
 			}
 
 			each(template.attributes, function(attr) {
-				if(attr.name.match(name_regex)) {
-					context[attr.value] = getInputValueConstraint(element);
-				} else if((on_regex_match = attr.name.match(on_regex))) {
+				var name = attr.name, value = attr.value;
+				if(name.match(name_regex)) {
+					context[value] = getInputValueConstraint(element);
+				} else if((on_regex_match = name.match(on_regex))) {
 					var event_name = on_regex_match[2];
-					aEL(element, event_name, context[attr.value]);
+					aEL(element, event_name, context[value]);
 				} else {
-					var constraint = get_constraint(attr.value, context, lineage);
+					var constraint = get_constraint(value, context, lineage);
 					if(is_constraint(constraint)) {
 						if(attr.name === "class") {
 							bindings.push(class_binding(element, constraint));
 						} else {
-							bindings.push(attr_binding(element, attr.name, constraint));
+							bindings.push(attr_binding(element, name, constraint));
 						}
 					} else {
 						element.setAttribute(attr.name, constraint);
