2970f0cb507fe3d51df740e070227089a03a0ce3
Steve Oney <swloney@gmail.com>
2018-07-14 23:06:26 -0400

    bump version

--- build/cjs.js
+++ build/cjs.js
@@ -1,4 +1,4 @@
-//     ConstraintJS (CJS) 0.9.7-beta
+//     ConstraintJS (CJS) 0.9.7
 //     ConstraintJS may be freely distributed under the MIT License
 //     http://cjs.from.so/
 
@@ -2114,7 +2114,7 @@ extend(cjs, {
 	 * @property {string} cjs.version
 	 * @see cjs.toString
 	 */
-	version: "0.9.7-beta", // This template will be filled in by the builder
+	version: "0.9.7", // This template will be filled in by the builder
 
 	/**
 	 * Print out the name and version of ConstraintJS
